45ae180db7b251ed5934f7536d674f7f6c37c0a8
athlon85 <smile@dgmong.com>
2018-06-29 13:30:47 +0900

    1234

--- www/compass.js
+++ www/compass.js
@@ -4,14 +4,14 @@ compass.prototype.getPosition = function(successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'compass', 'echo');
 };
 
-//
-//compass.install = function() {
-//    if (!window.plugins) {
-//        window.plugins = {};
-//    }
-//
-//    window.plugins.compass = new compass();
-//    return window.plugins.compass;
-//};
-//
-//cordova.addConstructor(compass.install);
+
+compass.install = function() {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+
+    window.plugins.compass = new compass();
+    return window.plugins.compass;
+};
+
+cordova.addConstructor(compass.install);
